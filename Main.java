f4ff327378e7b272863b942fd17b4502e6e48978
Tohasim <toreh.simonsen@live.dk>
2024-02-19 15:42:06 +0100

    Heuristic search done

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -17,17 +19,20 @@ class MazeSolver {
     int startCol;
     String[] maze;
     List<Coordinate> path;
+    Coordinate goal;
+    CoordinateComparator comparator;
 
     public void run() {
         try {
+            comparator = new CoordinateComparator();
             path = new ArrayList<>();
-            String content = new String(Files.readAllBytes(Paths.get("Maze3.txt")));
+            String content = new String(Files.readAllBytes(Paths.get("Maze2.txt")));
             content = content.replaceAll("\r", "");
             maze = content.split("\n");
             maze = translateWalls(maze);
             Coordinate startCoordinate = new Coordinate(startRow, startCol);
             printMaze();
-            startSearch(startCoordinate);
+            heuristicSearch(startCoordinate);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -44,6 +49,9 @@ class MazeSolver {
                     if (maze[row].toCharArray()[col] == 'S') {
                         startRow = row;
                         startCol = col;
+                    }
+                    if(maze[row].toCharArray()[col] == 'G'){
+                        goal = new Coordinate(row, col);
                     }
                     newRow.append(maze[row].toCharArray()[col]);
                 }
@@ -120,7 +128,31 @@ class MazeSolver {
         };
     }
 
-    private boolean startSearch(Coordinate currentCoordinate) {
+    private boolean heuristicSearch(Coordinate currentCoordinate){
+        if (currentCoordinate.isEqual(goal)) {
+            System.out.println(path);
+            showSolution(path);
+            System.out.println("Goal found in " + path.size() + " steps");
+            return true;
+        }
+        List<Coordinate> nextSteps = findAllNextSteps(currentCoordinate);
+        if (nextSteps.size() == 0) {
+            path.remove(currentCoordinate);
+            return false;
+        }
+        nextSteps.sort(new CoordinateComparator());
+        for (Coordinate nextStep : nextSteps) {
+            path.add(nextStep);
+            if (heuristicSearch(nextStep)) {
+                return true;
+            }
+        }
+        path.remove(currentCoordinate);
+        return false;
+
+    }
+
+    private boolean depthFirstSearch(Coordinate currentCoordinate) {
         int row = currentCoordinate.row;
         int col = currentCoordinate.col;
         if (maze[row].charAt(col) == 'G') {
@@ -136,7 +168,7 @@ class MazeSolver {
         }
         for (Coordinate nextStep : nextSteps) {
             path.add(nextStep);
-            if (startSearch(nextStep)) {
+            if (depthFirstSearch(nextStep)) {
                 return true;
             }
         }
@@ -197,7 +229,7 @@ class MazeSolver {
     }
 
 
-    class Coordinate {
+    static class Coordinate {
         int row, col;
         List<Coordinate> cameFrom;
 
@@ -213,6 +245,9 @@ class MazeSolver {
             this.cameFrom = cameFrom;
         }
 
+        public boolean isEqual(Coordinate other) {
+            return other.row == this.row && other.col == this.col;
+        }
 
         @Override
         public String toString() {
@@ -227,6 +262,13 @@ class MazeSolver {
                     return true;
             }
             return false;
+        }
+    }
+
+    class CoordinateComparator implements Comparator<MazeSolver.Coordinate>{
+        @Override
+        public int compare(MazeSolver.Coordinate o1, MazeSolver.Coordinate o2) {
+            return Math.abs(o1.row - goal.row) - Math.abs(o1.col - goal.col);
         }
     }
 }
